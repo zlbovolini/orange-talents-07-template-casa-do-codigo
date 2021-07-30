650dc398008a98634c20cda258fa8899c671a3ce
Lucas Bovolini <lucas.bovolini@zup.com.br>
2021-07-30 04:02:04 -0300

    Corrigido desserializacao do tipo LocalDate pelo construtor

--- src/main/java/com/github/zlbovolini/casacodigo/book/BookRequest.java
+++ src/main/java/com/github/zlbovolini/casacodigo/book/BookRequest.java
@@ -1,5 +1,6 @@
 package com.github.zlbovolini.casacodigo.book;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.github.zlbovolini.casacodigo.author.Author;
 import com.github.zlbovolini.casacodigo.category.Category;
@@ -10,6 +11,8 @@ import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.function.BiFunction;
+
+import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
 
 public class BookRequest {
 
@@ -50,8 +53,9 @@ public class BookRequest {
     @Exists(entity = Author.class)
     private Long authorId;
 
+    @JsonCreator(mode = PROPERTIES)
     public BookRequest(String title, String resume, String summary, BigDecimal price,
-                       Integer pages, String isbn,
+                       Integer pages, String isbn, LocalDate publishedOn,
                        Long categoryId, Long authorId) {
         this.title = title;
         this.resume = resume;
@@ -59,7 +63,7 @@ public class BookRequest {
         this.price = price;
         this.pages = pages;
         this.isbn = isbn;
-        //this.publishedOn = publishedOn;
+        this.publishedOn = publishedOn;
         this.categoryId = categoryId;
         this.authorId = authorId;
     }
@@ -72,11 +76,4 @@ public class BookRequest {
         return new Book(title, resume, summary, price, pages, isbn, publishedOn, category, author);
     }
 
-    /**
-     * Necessário para realizar a desserializaçãoo da data, pois pelo construtor não funciona.
-     * @param publishedOn
-     */
-    public void setPublishedOn(LocalDate publishedOn) {
-        this.publishedOn = publishedOn;
-    }
 }
